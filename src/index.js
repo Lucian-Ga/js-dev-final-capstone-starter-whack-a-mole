1e0288a5a6680ca5d80dd05a107d405c741b9947
Lucian-Ga <luciangaroiu@gmail.com>
2024-05-30 15:11:44 +0900

    small change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const startButton = document.querySelector('#start');
 const score; // Use querySelector() to get the score element
 const timerDisplay; // use querySelector() to get the timer element.
 
-let time = 0;
+let times = 0;
 let timer;
 let lastHole = 0;
 let points = 0;
