6972abb48a62c58ecd7ceeb8f3ba9948c342dafe
Lucian-Ga <luciangaroiu@gmail.com>
2024-05-30 16:57:20 +0900

    Added US-03 code

--- src/index.js
+++ src/index.js
@@ -66,7 +66,11 @@ function setDelay(difficulty) {
 function chooseHole(holes) {
   let index = randomInteger(0, 8);
   let hole = holes[index];
-  hole === lastHole ? chooseHole(holes) : (lastHole = hole);
+  if (hole === lastHole) {
+    chooseHole(holes);
+  } else if ((lastHole = hole)) {
+    return hole;
+  }
 }
 
 /**
@@ -90,7 +94,13 @@ function chooseHole(holes) {
  *
  */
 function gameOver() {
-  // TODO: Write your code here
+  if (time > 0) {
+    timeoutId = showUp();
+    return timeoutId;
+  } else {
+    gameStopped = stopGame();
+    return gameStopped;
+  }
 }
 
 /**
@@ -103,8 +113,8 @@ function gameOver() {
  *
  */
 function showUp() {
-  let delay = 0; // TODO: Update so that it uses setDelay()
-  const hole = 0; // TODO: Update so that it use chooseHole()
+  let delay = setDelay(difficulty); // TODO: Update so that it uses setDelay()
+  const hole = chooseHole(holes); // TODO: Update so that it use chooseHole()
   return showAndHide(hole, delay);
 }
 
@@ -118,12 +128,14 @@ function showUp() {
  */
 function showAndHide(hole, delay) {
   // TODO: call the toggleVisibility function so that it adds the 'show' class.
+  toggleVisibility(hole);
 
   const timeoutID = setTimeout(() => {
     // TODO: call the toggleVisibility function so that it removes the 'show' class when the timer times out.
+    toggleVisibility(hole);
 
     gameOver();
-  }, 0); // TODO: change the setTimeout delay to the one provided as a parameter
+  }, delay); // TODO: change the setTimeout delay to the one provided as a parameter
   return timeoutID;
 }
 
@@ -135,6 +147,7 @@ function showAndHide(hole, delay) {
  */
 function toggleVisibility(hole) {
   // TODO: add hole.classList.toggle so that it adds or removes the 'show' class.
+  hole.classList.toggle("show");
 
   return hole;
 }
@@ -248,8 +261,8 @@ function stopGame() {
  *
  */
 function startGame() {
-  //setDuration(10);
-  //showUp();
+  setDuration(10);
+  showUp();
   return "game started";
 }
 
