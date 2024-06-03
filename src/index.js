49c7a907a8f57c9c7274ffad934259d7571aa141
Lucian-Ga <luciangaroiu@gmail.com>
2024-06-03 10:35:33 +0900

    Refactored code, updated assets, fixed issues

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const holes = document.querySelectorAll(".hole");
 const moles = document.querySelectorAll(".mole");
 const startButton = document.querySelector("#start");
-// TODO: Add the missing query selectors:
-const score = document.querySelector("#score"); // Use querySelector() to get the score element
-const timerDisplay = document.querySelector("#timer"); // use querySelector() to get the timer element.
+const score = document.querySelector("#score");
+const timerDisplay = document.querySelector("#timer");
 
 let time = 0;
 let timer;
@@ -113,8 +112,8 @@ function gameOver() {
  *
  */
 function showUp() {
-  let delay = setDelay(difficulty); // TODO: Update so that it uses setDelay()
-  const hole = chooseHole(holes); // TODO: Update so that it use chooseHole()
+  let delay = setDelay(difficulty);
+  const hole = chooseHole(holes);
   return showAndHide(hole, delay);
 }
 
@@ -127,15 +126,13 @@ function showUp() {
  *
  */
 function showAndHide(hole, delay) {
-  // TODO: call the toggleVisibility function so that it adds the 'show' class.
   toggleVisibility(hole);
 
   const timeoutID = setTimeout(() => {
-    // TODO: call the toggleVisibility function so that it removes the 'show' class when the timer times out.
     toggleVisibility(hole);
 
     gameOver();
-  }, delay); // TODO: change the setTimeout delay to the one provided as a parameter
+  }, delay);
   return timeoutID;
 }
 
@@ -146,9 +143,7 @@ function showAndHide(hole, delay) {
  *
  */
 function toggleVisibility(hole) {
-  // TODO: add hole.classList.toggle so that it adds or removes the 'show' class.
   hole.classList.toggle("show");
-
   return hole;
 }
 
